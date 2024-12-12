4b585d7683c736f8b8ad53853e714fb7a1cd0e25
Kayfresh <madubuike1573080@gmail.com>
2024-12-12 11:47:03 +0100

    Manage web with mobile only responsiveness

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+const openNav = document.querySelector('.bi-list');
+
+const closeNav = document.querySelector('.bi-x-lg');
+
+const navList = document.querySelector('.nav-ul');
+
+openNav.addEventListener('click', () => {
+  openNav.style.display = 'none';
+  closeNav.style.display = 'block';
+  navList.style.display = 'block';
+});
+
+closeNav.addEventListener("click", () => {
+  openNav.style.display = "block";
+  closeNav.style.display = "none";
+  navList.style.display = "none";
+});
+
+// navList.addEventListener()
